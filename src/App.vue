9d1a2c369b8f8a26283f5363b26f7b96fa5b6e05
João Henrique <santos.joao@crmsolucoes.com.br>
2023-12-29 10:44:21 -0300

    3 - Diferenças entre v-if e v-show

--- src/App.vue
+++ src/App.vue
@@ -1,10 +1,20 @@
 <template>
-  <headerComponent/>
-  <img alt="Vue logo" src="./assets/logo.png">
+  
+  <div>
+    <headerComponent v-show="statusHeader"/>
+    <button @click="showHeader">Mostrar/Ocultar</button>
+  </div>
+
+  <div v-if="accessLevel == 1">Admin</div>
+  <div v-else-if="accessLevel == 2">Marketing</div>
+  <div v-else>User</div>
+
+  <!-- <img alt="Vue logo" src="./assets/logo.png"> -->
   <HelloWorld msg="Welcome to Your Vue.js App"/>
 </template>
 
 <script>
+
 import headerComponent from './components/headerComponent,.vue'
 import HelloWorld from './components/HelloWorld.vue'
 
@@ -14,8 +24,29 @@ export default {
     HelloWorld,
     headerComponent
   },
+
   mounted() {
     document.title = 'Aulas Vue.js 3'
+  },
+
+  //  A função "data()" sempre retorna um objeto, sendo que cada item se torna um variável utilizável
+  data(){
+    return {
+      statusHeader: true,
+      accessLevel: 2,
+    }
+  },
+
+  methods: {
+    
+    showHeader: function(){
+      if(this.statusHeader == true){
+        this.statusHeader = false
+      }else{
+        this.statusHeader = true
+      }
+    }
+
   }
 }
 </script>
