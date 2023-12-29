e48c6129909caa84f1c5bfb1e829a7b5f5520ede
João Henrique <santos.joao@crmsolucoes.com.br>
2023-12-29 11:09:43 -0300

    4 - A diretiva v-for

--- src/App.vue
+++ src/App.vue
@@ -1,27 +1,16 @@
 <template>
   
-  <div>
-    <headerComponent v-show="statusHeader"/>
-    <button @click="showHeader">Mostrar/Ocultar</button>
-  </div>
+  <headerComponent/>
 
-  <div v-if="accessLevel == 1">Admin</div>
-  <div v-else-if="accessLevel == 2">Marketing</div>
-  <div v-else>User</div>
-
-  <!-- <img alt="Vue logo" src="./assets/logo.png"> -->
-  <HelloWorld msg="Welcome to Your Vue.js App"/>
 </template>
 
 <script>
 
 import headerComponent from './components/headerComponent,.vue'
-import HelloWorld from './components/HelloWorld.vue'
 
 export default {
   name: 'App',
   components: {
-    HelloWorld,
     headerComponent
   },
 
@@ -32,32 +21,18 @@ export default {
   //  A função "data()" sempre retorna um objeto, sendo que cada item se torna um variável utilizável
   data(){
     return {
-      statusHeader: true,
-      accessLevel: 2,
+      
     }
   },
 
   methods: {
     
-    showHeader: function(){
-      if(this.statusHeader == true){
-        this.statusHeader = false
-      }else{
-        this.statusHeader = true
-      }
-    }
+    
 
   }
 }
 </script>
 
 <style>
-#app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
-}
+
 </style>
--- src/components/headerComponent,.vue
+++ src/components/headerComponent,.vue
@@ -1,9 +1,51 @@
 <template>
-  <header>Header</header>
+  
+  <div v-for="(item, index) in todos" :key="item.id">
+    <p>Index - {{ index }}</p>
+    <p>Título - {{ item.title }}</p>
+  </div>
+  
 </template>
 
 <script>
 export default {
+
+  data() {
+    return {
+      todos: [
+        {
+          "userId": 1,
+          "id": 1,
+          "title": "delectus aut autem",
+          "completed": false
+        },
+        {
+          "userId": 1,
+          "id": 2,
+          "title": "quis ut nam facilis et officia qui",
+          "completed": false
+        },
+        {
+          "userId": 1,
+          "id": 3,
+          "title": "fugiat veniam minus",
+          "completed": false
+        },
+        {
+          "userId": 1,
+          "id": 4,
+          "title": "et porro tempora",
+          "completed": true
+        },
+        {
+          "userId": 1,
+          "id": 5,
+          "title": "laboriosam mollitia et enim quasi adipisci quia provident illum",
+          "completed": false
+        }
+      ]
+    }
+  }
 
 }
 </script>
