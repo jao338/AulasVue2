004b970912372c71fa772994488a32d76ae46cbc
João Henrique <santos.joao@crmsolucoes.com.br>
2024-01-02 10:07:52 -0300

    11 - Ciclo de vida de um componente e os métodos envolvidos

--- src/App.vue
+++ src/App.vue
@@ -1,6 +1,12 @@
 <template>
   
-  <headerComponent/>
+<div id="app">
+  <headerComponent v-if="showHeader"/>
+
+  <br>
+  <button @click="showHeader = !showHeader">Ativar/Desativar header</button>
+
+</div>
 
 </template>
 
@@ -14,15 +20,43 @@ export default {
     headerComponent
   },
 
-  mounted() {
-    document.title = 'Aulas Vue.js 3'
-  },
-
-  //  A função "data()" sempre retorna um objeto, sendo que cada item se torna um variável utilizável
   data(){
     return {
-      
+      name: 'AAAAAAAAA',
+      showHeader: true,
     }
+  },
+
+  // "this.$el" retorna o elemento raiz do componente, no caso a div de id "app"
+  //  Executado antes de atualizar o componente
+  beforeUpdate(){
+    console.log("beforeUpdate -", this.name, ", DOM -", this.$el)
+  },
+
+  //  Executado após atualizar o componente
+  updated(){
+    console.log("updated -", this.name, ", DOM -", this.$el)
+  },
+
+  //  Executado antes de criar o componente
+  beforeCreate(){
+    console.log("beforeCreate -", this.name, ", DOM -", this.$el)
+  },
+
+  //  Executado após criar o componente
+  created(){
+    console.log("created -", this.name, ", DOM -", this.$el)
+  },
+
+  //  Executado antes de montar o componente
+  beforeMount(){
+    console.log("beforeMount -", this.name, ", DOM -", this.$el)
+  },
+
+  //  Executado após montar o componente
+  mounted() {
+    console.log("mounted -", this.name, ", DOM -", this.$el)
+    document.title = 'Aulas Vue.js 3'
   },
 
 }
--- src/components/headerComponent,.vue
+++ src/components/headerComponent,.vue
@@ -1,29 +1,10 @@
 <template>  
-<div>
 
-  <label>Escreva um nome: </label><br><br>
-  <input type="text" v-model="name"><br><br>
+<div id="app">
+
+  <h1>Header</h1>
+
   <span>{{ name }}</span>
-
-  <br><br><br>
-
-  <label>Escreva o primeiro nome: </label><br><br>
-  <input type="text" v-model="user.firstName"><br><br>
-  <label>Escreva o último nome: </label><br><br>
-  <input type="text" v-model="user.lastName"><br><br>
-  <span>{{ user.firstName }} {{ user.lastName }}</span>
-
-  <br><br><br>
-
-  <div>Escolha um valor: </div><br>
-  <select v-model="pageCount">
-
-    <option value="5">5</option>
-    <option value="10">10</option>
-    <option value="15">15</option>
-
-  </select><br><br>
-  <span>{{ pageCount }}</span>
 
 </div>
   
@@ -34,48 +15,29 @@ export default {
 
   data() {
     return {
-      name: "",
-      pageCount: 5,
-      user: {
-        firstName: "",
-        lastName: ""
-      }
-    }
-  },
-
-  computed: {
-  },  
-
-  //  O objeto "watch" possui uma coletânea de itens, sendo que cada item é uma variável definida em "data()" e sempre com o mesmo nome. Toda vez que uma variável que está dentro de "watch" é alterada, uma rotina é correspondente é executada.
-
-  //  Ao observar um objeto é necessário atribuir uma sintaxe diferente, ao invés ds declaração de método, é declarado um objeto contendo dois itens. Um deles é o "handler" que é onde fica a rotina de comandos e o outro é "deep: true", que significa que sempre que um item do objeto que está sendo observado se alterar, a rotina de comandos deve ser executada
-  watch: {
-    name(){
-      this.saveUserName()
-    },
-
-    pageCount(){
-      this.changePageCount()
-    },
-
-    user: {
-      handler() {
-        console.log("User alterado")
-      },
-
-      deep: true
-
+      name: 'AAAAAAAAA',
     }
   },
 
   methods: {
-    saveUserName(){
-      console.log(this.name);
-    },
+    
+  },
 
-    changePageCount(){
-      console.log(this.pageCount);
-    }
+  watch: {
+
+  },
+
+  computed: {
+
+  },
+
+  // "this.$el" retorna o elemento raiz do componente, no caso a div de id "app"
+  beforeUnmount() {
+    console.log("beforeUnmount -", this.name, ", DOM -", this.$el)
+  },
+
+  unmounted(){
+    console.log("unmounted -", this.name, ", DOM -", this.$el)
   }
 
 }
